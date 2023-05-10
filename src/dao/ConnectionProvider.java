7024ae93c3f447f9f2abf96f5f5a721d9fc75992
Lê Chí Dũng <dung.lc@sis.hust.edu.vn>
2023-05-10 13:05:22 +0700

    da inu

--- src/dao/ConnectionProvider.java
+++ src/dao/ConnectionProvider.java
@@ -5,6 +5,7 @@
 package dao;
 
 import java.sql.*;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -15,10 +16,15 @@ public class ConnectionProvider {
 		String url = "jdbc:sqlserver://database-2.cglnwe68wxsl.ap-southeast-2.rds.amazonaws.com:1433;databaseName=CafeManagement";
 		String user = "admin";
 		String password = "12345678";
+                        System.out.println(123);
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			return DriverManager.getConnection(url, user, password);
+			Connection con = DriverManager.getConnection(url, user, password);
+                        System.out.println(con);
+                        return con;
 		} catch (ClassNotFoundException | SQLException e) {
+                    e.printStackTrace();
+                    JOptionPane.showMessageDialog(null, e, "Message", JOptionPane.ERROR_MESSAGE);
 			return null;
 		}
 
--- src/dao/tables.java
+++ src/dao/tables.java
@@ -1,23 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package dao;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Dungpc
- */
 public class tables {
 	public static void main(String[] args) {
 		try {
-			String userTable = " CREATE TABLE myTable " + "( id INT IDENTITY(1,1) PRIMARY KEY," + "  name VARCHAR(200),"
+			/*String userTable = " CREATE TABLE myTable " + "( id INT IDENTITY(1,1) PRIMARY KEY," + "  name VARCHAR(200),"
 					+ "  email VARCHAR(200) UNIQUE," + "  mobilenumber VARCHAR(200)," + "  address VARCHAR(200),"
-					+ " password VARCHAR(200)," + " securityQuestion VARCHAR(200)," + " answer VARCHAR(200) )";
-			DbOperations.setDataOrDelete(userTable, "Create Table successfully");
+					+ " password VARCHAR(200)," + " securityQuestion VARCHAR(200)," + " answer VARCHAR(200) ),"+"status VARCHAR(200)";
+			DbOperations.setDataOrDelete(userTable, "Create Table successfully");*/
 
+                        String adminDetails = "INSERT INTO myTable(name,email,mobilenumber,address,password,securityQuestion,answer,status) "
+                                + " VALUES('dung','ledung200803@gmail.com','0335620803','Hanoi','12345678','What is your girlfriend name?','Phuong','true')";
+                        DbOperations.setDataOrDelete(adminDetails, " Add admin details successfully");
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e);
 			e.printStackTrace();
